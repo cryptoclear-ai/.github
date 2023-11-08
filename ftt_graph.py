70f47c7a371bb890011d7719799dc829e70f9d12
DebugScientist78 <chency.wang@gmail.com>
2023-11-07 22:52:15 -0800

    updated attribute scripts

--- ftt_graph.py
+++ ftt_graph.py
@@ -3,17 +3,18 @@ import torch
 from torch_geometric.data import Data
 import torch_geometric.utils
 from datetime import datetime
+from datetime import timedelta
 from collections import OrderedDict
 import networkx as nx
 import matplotlib.pyplot as plt
+import time
+import os
 
-filename = input()
+filename = input("transaction file (exclude extension)")
 transaction = pd.read_csv(filename+".csv")
+print("file loaded")
+start = time.time()
 
-start = datetime.strptime(transaction.iloc[0]["block_timestamp"][:10], "%Y-%m-%d")
-end = datetime.strptime(transaction.iloc[-1]["block_timestamp"][:10], "%Y-%m-%d")
-
-date_range = pd.date_range(start, end)
 raw_data = OrderedDict()
 raw_money_sent = OrderedDict()
 raw_node_recieved = OrderedDict()
@@ -29,19 +30,15 @@ edge_dict = {}
 
 def write_to_row(previous_timestamp):
 	print(previous_timestamp)
-	x = []
-	edges = []
-	for key in dictionary:
-		x.append([dictionary[key]])
-	for pair in edge_dict:
-		edges.append([dictionary[pair[0]] , dictionary[pair[1]]])
+	
+	x = list(dictionary.values())
+	edges = [[dictionary[pair[0]], dictionary[pair[1]]] for pair in edge_dict]
+
 	edge_index = torch.tensor(edges, dtype=torch.long)
 	data = Data(num_nodes=len(x), x=x, edge_index=edge_index.t().contiguous())
 	g = torch_geometric.utils.to_networkx(data, to_undirected=False)
-	node_edge = {}
-	for pair in edge_dict:
-		new_pair = (dictionary[pair[0]], dictionary[pair[1]])
-		node_edge[new_pair] = edge_dict[pair]
+	g.remove_edges_from(nx.selfloop_edges(g))
+	node_edge = {(dictionary[pair[0]], dictionary[pair[1]]): edge_dict[pair] for pair in edge_dict}
 
 	nx.set_edge_attributes(g, node_edge)
 
@@ -89,131 +86,82 @@ def write_to_row(previous_timestamp):
 		raw_node_recieved[node_to_addr[node]]["Address"] = node_to_addr[node]
 		raw_node_recieved[node_to_addr[node]][previous_timestamp.strftime("%Y/%m/%d")] = len(sent_map[node_to_addr[node]])
 
-previous_timestamp = datetime.strptime(transaction.iloc[0]["block_timestamp"][:10], "%Y-%m-%d")
-for index, row in transaction.iterrows():
-	timestamp = datetime.strptime(row.get("block_timestamp")[:10], "%Y-%m-%d")
-	from_addr = row.get("from_address")
-	to_addr = row.get("to_address")
-
-	#print("now", timestamp)
-	#print("before", previous_timestamp)
-	if (timestamp > previous_timestamp):
-		write_to_row(previous_timestamp)
-
-	if from_addr not in dictionary:
-		dictionary[from_addr] = count
-		node_to_addr[count] = from_addr
-		count += 1
-	if to_addr not in dictionary:
-		dictionary[to_addr] = count
-		node_to_addr[count] = to_addr
-		count += 1
-
-	pair = (from_addr, to_addr)
-	attr = {}
-	if pair not in edge_dict:
-		attr["weight"] = int(row.get("value"))
-		edge_dict[pair] = attr
-	else:
-		edge_dict[pair]["weight"] += int(row.get("value"))
-	previous_timestamp = timestamp
-
-write_to_row(previous_timestamp)
-
-def write_csv(filename, mp):
-	ls = []
-	for row in mp:
-		ls.append(mp[row])
+def write_csv(filename, mp, folder):
+	try:
+		os.mkdir(folder)
+	except:
+		#folder already exists
+		pass
+	ls = [mp[row] for row in mp]
 	df = pd.DataFrame(ls)
-	df.to_csv(filename)
+	df.to_csv(f"{folder}/{filename}")
 
-write_csv(filename+"_num_nodes_sent.csv", raw_data)
-write_csv(filename+"_num_nodes_recieved.csv", raw_node_recieved)
-write_csv(filename+"_money_sent.csv", raw_money_sent)
-write_csv(filename+"_money_received.csv", raw_money_recieved)
-
-
-'''
-def get_df_uptill(dt, start_index):
-	sub_df = transaction[start_index:]
-	for index, row in sub_df.iterrows():
-		date = datetime.strptime(row.get("block_timestamp")[:10], "%Y-%m-%d")
-		delta = dt - date
-		if delta.days < 0:
-			cutoff = int(index)
-			#print(cutoff)
-			return sub_df[:cutoff], cutoff+start_index
-	return sub_df, len(transaction)
-'''
-#print(ftt)
-#print(get_df_uptill(end))
-'''
-dictionary = {}
-
-node_to_addr = {}
-
-count = 0
-edge_dict ={}
-
-def getGraph(df,c):
+def read_data_frame(df, year_range, c):
 	count = c
+	previous_timestamp = df.iloc[0]["block_timestamp"].to_pydatetime()
 	for index, row in df.iterrows():
+		timestamp = row.get("block_timestamp").to_pydatetime()
 		from_addr = row.get("from_address")
-		if from_addr in dictionary:
-			pass
-		else:
-			dictionary[from_addr] = count
-			count += 1
 		to_addr = row.get("to_address")
-		if to_addr in dictionary:
-			pass
-		else:
-			dictionary[to_addr] = count
+
+		#print("now", timestamp)
+		#print("before", previous_timestamp)
+		if (timestamp.date() > previous_timestamp.date()):
+			write_to_row(previous_timestamp)
+
+		if from_addr not in dictionary:
+			dictionary[from_addr] = count
+			node_to_addr[count] = from_addr
 			count += 1
+		if to_addr not in dictionary:
+			dictionary[to_addr] = count
+			node_to_addr[count] = to_addr
+			count += 1
+
 		pair = (from_addr, to_addr)
+		attr = {}
 		if pair not in edge_dict:
-			edge_dict[pair] = int(row.get("value"))
+			attr["weight"] = int(row.get("value"))
+			edge_dict[pair] = attr
 		else:
-			edge_dict[pair] += int(row.get("value"))
-	
-	#print(dictionary)
-	x = []
-	for key in dictionary:
-		x.append([dictionary[key]])
-		node_to_addr[dictionary[key]] = key
-	#print(node_to_addr)
-	
-	edges = []
-	for pair in edge_dict:
-		edges.append([dictionary[pair[0]], dictionary[pair[1]]])
+			edge_dict[pair]["weight"] += int(row.get("value"))
+		previous_timestamp = timestamp
 
-	edge_index = torch.tensor(edges, dtype=torch.long)
-	data = Data(num_nodes=len(x), x=x, edge_index=edge_index.t().contiguous())
-	return data,count
+	write_to_row(previous_timestamp)
 
-start_index = 0
-for dt in date_range:
-	dt_o = dt.to_pydatetime()
-	print(dt_o.strftime("%Y/%m/%d"))
-	#try:
-	sub_df, start_index = get_df_uptill(dt_o, start_index)
-	data,count = getGraph(sub_df, count)
-	#print(node_to_addr)
-	#print(dictionary)
-	g = torch_geometric.utils.to_networkx(data, to_undirected=False)
-	#print(g.nodes)
-	for node in g.nodes():
-		num_adj_nodes = len(list(g.neighbors(node)))
-		if node_to_addr[node] not in raw_data:
-			raw_data[node_to_addr[node]] = {}
-		raw_data[node_to_addr[node]]["Address"] = node_to_addr[node]
-		raw_data[node_to_addr[node]][dt_o.strftime("%Y/%m/%d")] = num_adj_nodes
-	#except Exception as e:
-		#print(str(e))
+	write_csv(f"{filename}_{year_range}_num_nodes_sent.csv", raw_data, "nodes_sent_attribute_3")
+	write_csv(f"{filename}_{year_range}_num_nodes_recieved.csv", raw_node_recieved, "nodes_recieved_attribute_4")
+	write_csv(f"{filename}_{year_range}_money_sent.csv", raw_money_sent, "total_money_sent_attribute_5")
+	write_csv(f"{filename}_{year_range}_money_received.csv", raw_money_recieved, "total_money_recieved_attribute_6")
+	return count
 
-lis = []
-for row in raw_data:
-	lis.append(raw_data[row])
+def partition_by_year(df, c):
+	count = c
+	start = datetime.strptime(df.iloc[0]["block_timestamp"][:10], "%Y-%m-%d")
+	years = set([int(df.iloc[x]["block_timestamp"][:4]) for x, r in df.iterrows()])
+	print(filename, "contains:", years)
 
-#print(lis)
-'''
+	df['block_timestamp'] = pd.to_datetime(df['block_timestamp'])
+	df = df.sort_values('block_timestamp', ignore_index=True)
+
+	for yr in years:
+		print("Starting:", yr)
+		data = df.loc[pd.to_datetime(df['block_timestamp']).dt.year == yr]
+		#print(type(data))
+
+		global raw_data
+		global raw_money_sent
+		global raw_node_recieved
+		global raw_money_recieved
+
+		raw_data = OrderedDict()
+		raw_money_sent = OrderedDict()
+		raw_node_recieved = OrderedDict()
+		raw_money_recieved = OrderedDict();
+		count = read_data_frame(data, f"{int(yr)}_{int(yr)+1}", count)
+		print("Finished:", yr)
+
+partition_by_year(transaction, count)
+end = time.time()
+elapsed = end - start
+print(str(timedelta(seconds=elapsed)))
